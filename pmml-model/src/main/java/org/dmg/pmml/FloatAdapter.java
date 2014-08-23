38cc622c6fd0f57546e0e977c4ff89a89eb0d39d
Villu Ruusmann <villu.ruusmann@gmail.com>
2014-08-24 00:10:58 +0300

    Changed the Java representation of the XSD 'float' data type from java.lang.Float to java.lang.Double

---
+++ pmml-model/src/main/java/org/dmg/pmml/FloatAdapter.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2014 Villu Ruusmann
+ */
+package org.dmg.pmml;
+
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class FloatAdapter extends XmlAdapter<String, Double> {
+
+	@Override
+	public Double unmarshal(String value){
+		return Double.valueOf(DatatypeConverter.parseFloat(value));
+	}
+
+	@Override
+	public String marshal(Double value){
+
+		if(value == null){
+			return null;
+		}
+
+		return DatatypeConverter.printFloat(value.floatValue());
+	}
+}
