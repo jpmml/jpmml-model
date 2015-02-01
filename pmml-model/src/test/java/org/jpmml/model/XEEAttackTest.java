c2d25407ad3bc918c47951b1fdadd1d78e182d70
Villu Ruusmann <villu.ruusmann@gmail.com>
2015-02-01 13:12:33 +0200

    Added unit test for XML Entity Expansion (XEE) attacks (eg. the "billion laughs" attack)

---
+++ pmml-model/src/test/java/org/jpmml/model/XEEAttackTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015 Villu Ruusmann
+ */
+package org.jpmml.model;
+
+import java.io.InputStream;
+
+import javax.xml.bind.UnmarshalException;
+import javax.xml.transform.stream.StreamSource;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class XEEAttackTest {
+
+	@Test
+	public void unmarshal() throws Exception {
+		InputStream is = PMMLUtil.getResourceAsStream(XEEAttackTest.class);
+
+		try {
+			JAXBUtil.unmarshalPMML(new StreamSource(is));
+
+			fail();
+		} catch(UnmarshalException ue){
+			// Ignored
+		} finally {
+			is.close();
+		}
+	}
+}
