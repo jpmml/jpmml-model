cfaa1e188941ddcaf74e2730fafe13205dddfc7f
Villu Ruusmann <villu.ruusmann@gmail.com>
2015-07-18 19:20:19 +0300

    Optimized the unmarshalling of default values
    $ grep -r "[Integer|Decimal]Adapter()\.unmarshal" pmml-model/target/generated-sources/

--- pmml-model/src/main/java/org/dmg/pmml/adapters/DecimalAdapter.java
+++ pmml-model/src/main/java/org/dmg/pmml/adapters/DecimalAdapter.java
@@ -12,6 +12,15 @@ public class DecimalAdapter extends XmlAdapter<String, Double> {
 
 	@Override
 	public Double unmarshal(String value){
+
+		if("0.0".equals(value)){
+			return DecimalAdapter.ZERO;
+		} else
+
+		if("1.0".equals(value)){
+			return DecimalAdapter.ONE;
+		}
+
 		BigDecimal decimal = DatatypeConverter.parseDecimal(value);
 
 		if(!ValueUtil.checkRange(decimal, DecimalAdapter.MIN_VALUE, DecimalAdapter.MAX_VALUE)){
@@ -30,6 +39,9 @@ public class DecimalAdapter extends XmlAdapter<String, Double> {
 
 		return DatatypeConverter.printDecimal(BigDecimal.valueOf(value.doubleValue()));
 	}
+
+	public static final Double ZERO = Double.valueOf(0.0d);
+	public static final Double ONE = Double.valueOf(1.0d);
 
 	protected static final BigDecimal MIN_VALUE = BigDecimal.valueOf(-Double.MAX_VALUE);
 	protected static final BigDecimal MAX_VALUE = BigDecimal.valueOf(Double.MAX_VALUE);
--- pmml-model/src/main/java/org/dmg/pmml/adapters/IntegerAdapter.java
+++ pmml-model/src/main/java/org/dmg/pmml/adapters/IntegerAdapter.java
@@ -12,6 +12,15 @@ public class IntegerAdapter extends XmlAdapter<String, Integer> {
 
 	@Override
 	public Integer unmarshal(String value){
+
+		if("0".equals(value)){
+			return IntegerAdapter.ZERO;
+		} else
+
+		if("1".equals(value)){
+			return IntegerAdapter.ONE;
+		}
+
 		BigInteger integer = DatatypeConverter.parseInteger(value);
 
 		if(!ValueUtil.checkRange(integer, IntegerAdapter.MIN_VALUE, IntegerAdapter.MAX_VALUE)){
@@ -30,6 +39,9 @@ public class IntegerAdapter extends XmlAdapter<String, Integer> {
 
 		return DatatypeConverter.printInteger(BigInteger.valueOf(value.intValue()));
 	}
+
+	public static final Integer ZERO = Integer.valueOf(0);
+	public static final Integer ONE = Integer.valueOf(1);
 
 	protected static final BigInteger MIN_VALUE = BigInteger.valueOf(Integer.MIN_VALUE);
 	protected static final BigInteger MAX_VALUE = BigInteger.valueOf(Integer.MAX_VALUE);
