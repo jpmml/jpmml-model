7555eba14467292984a99c13ccdfc98d76b31eca
Villu Ruusmann <villu.ruusmann@gmail.com>
2016-01-21 16:23:44 +0200

    Declared XML namespace information
    EclipseLink MOXy uses the package-level @XmlSchema annotation for
    identifying the default XML namespace.

---
+++ pmml-model/src/main/java/org/dmg/pmml/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Villu Ruusmann
+ */
+@XmlSchema (
+	namespace = "http://www.dmg.org/PMML-4_2",
+	elementFormDefault = XmlNsForm.QUALIFIED
+)
+package org.dmg.pmml;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
