f261276d5a0a5d116679cdc12a1b937b88fe8960
Villu Ruusmann <villu.ruusmann@gmail.com>
2016-01-23 01:20:17 +0200

    Added utility method for pretty-printing PMML documents

---
+++ pmml-model-metro/src/main/java/org/jpmml/model/MetroJAXBUtil.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 Villu Ruusmann
+ */
+package org.jpmml.model;
+
+import java.io.OutputStream;
+
+import javax.xml.bind.JAXBException;
+
+import com.sun.xml.bind.v2.runtime.JAXBContextImpl;
+import com.sun.xml.bind.v2.runtime.MarshallerImpl;
+import com.sun.xml.bind.v2.runtime.output.XmlOutput;
+import org.dmg.pmml.PMML;
+
+public class MetroJAXBUtil {
+
+	private MetroJAXBUtil(){
+	}
+
+	static
+	public void marshalPMML(PMML pmml, OutputStream os) throws JAXBException {
+		JAXBContextImpl context;
+
+		try {
+			context = (JAXBContextImpl)JAXBUtil.getContext();
+		} catch(ClassCastException cce){
+			throw new IllegalStateException("Not a GlassFish Metro runtime", cce);
+		}
+
+		MarshallerImpl marshaller = context.createMarshaller();
+
+		XmlOutput xmlOutput = new PrettyUTF8XmlOutput(os, context.getUTF8NameTable());
+
+		marshaller.marshal(pmml, xmlOutput);
+	}
+}
---
+++ pmml-model-metro/src/main/java/org/jpmml/model/PrettyUTF8XmlOutput.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2016 Villu Ruusmann
+ */
+package org.jpmml.model;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+import javax.xml.stream.XMLStreamException;
+
+import com.sun.xml.bind.marshaller.MinimumEscapeHandler;
+import com.sun.xml.bind.v2.runtime.Name;
+import com.sun.xml.bind.v2.runtime.output.Encoded;
+import com.sun.xml.bind.v2.runtime.output.IndentingUTF8XmlOutput;
+import com.sun.xml.bind.v2.runtime.output.Pcdata;
+import com.sun.xml.bind.v2.runtime.output.UTF8XmlOutput;
+import org.xml.sax.SAXException;
+
+/**
+ * @see IndentingUTF8XmlOutput
+ */
+public class PrettyUTF8XmlOutput extends UTF8XmlOutput {
+
+	private int depth = 0;
+
+	private boolean textWritten = false;
+
+
+	public PrettyUTF8XmlOutput(OutputStream os, Encoded[] localNames){
+		super(os, localNames, MinimumEscapeHandler.theInstance);
+	}
+
+	@Override
+	public void endDocument(boolean fragment) throws IOException, SAXException, XMLStreamException {
+		write('\n');
+
+		super.endDocument(fragment);
+	}
+
+	@Override
+	public void beginStartTag(int prefix, String localName) throws IOException {
+		indentStartTag();
+
+		super.beginStartTag(prefix, localName);
+	}
+
+	@Override
+	public void beginStartTag(Name name) throws IOException {
+		indentStartTag();
+
+		super.beginStartTag(name);
+	}
+
+	@Override
+	public void endTag(int prefix, String localName) throws IOException {
+		indentEndTag();
+
+		super.endTag(prefix, localName);
+	}
+
+	@Override
+	public void endTag(Name name) throws IOException {
+		indentEndTag();
+
+		super.endTag(name);
+	}
+
+	@Override
+	public void text(String value, boolean needsSeparatingWhitespace) throws IOException {
+		super.text(value, needsSeparatingWhitespace);
+
+		this.textWritten = true;
+	}
+
+	@Override
+	public void text(Pcdata value, boolean needsSeparatingWhitespace) throws IOException {
+		super.text(value, needsSeparatingWhitespace);
+
+		this.textWritten = true;
+	}
+
+	private void indentStartTag() throws IOException {
+		closeStartTag();
+
+		if(!this.textWritten){
+			printIndent();
+		}
+
+		this.depth++;
+
+		this.textWritten = false;
+	}
+
+	private void indentEndTag() throws IOException {
+		this.depth--;
+
+		if(!super.closeStartTagPending && !this.textWritten){
+			printIndent();
+		}
+
+		this.textWritten = false;
+	}
+
+	private void printIndent() throws IOException {
+		write('\n');
+
+		int length = this.depth;
+
+		while(length > PrettyUTF8XmlOutput.indent.length){
+			write(PrettyUTF8XmlOutput.indent);
+
+			length -= PrettyUTF8XmlOutput.indent.length;
+		}
+
+		write(PrettyUTF8XmlOutput.indent, 0, length);
+	}
+
+	private static final byte[] indent = new byte[1024];
+
+	static {
+		Arrays.fill(PrettyUTF8XmlOutput.indent, (byte)'\t');
+	}
+}
