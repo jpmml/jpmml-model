d4daeeb02b21bf17b07f99af2f887c8e4e669995
Villu Ruusmann <villu.ruusmann@gmail.com>
2018-06-07 11:10:21 +0300

    Improved exception messages

--- pmml-model/src/main/java/org/dmg/pmml/Version.java
+++ pmml-model/src/main/java/org/dmg/pmml/Version.java
@@ -3,6 +3,9 @@
  */
 package org.dmg.pmml;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum Version {
 	PMML_3_0("http://www.dmg.org/PMML-3_0"),
 	PMML_3_1("http://www.dmg.org/PMML-3_1"),
@@ -31,7 +34,9 @@ public enum Version {
 	public String getVersion(){
 		String namespaceURI = getNamespaceURI();
 
-		return (namespaceURI.substring("http://www.dmg.org/PMML-".length())).replace('_', '.');
+		String version = namespaceURI.substring("http://www.dmg.org/PMML-".length());
+
+		return version.replace('_', '.');
 	}
 
 	static
@@ -59,6 +64,13 @@ public enum Version {
 			}
 		}
 
-		throw new IllegalArgumentException(namespaceURI);
+		Matcher matcher = Version.PATTERN_XMLNS.matcher(namespaceURI);
+		if(!matcher.matches()){
+			throw new IllegalArgumentException("PMML namespace URI " + namespaceURI + " does not match \'" + Version.PATTERN_XMLNS.pattern() + "\' regex pattern");
+		}
+
+		throw new IllegalArgumentException("PMML namespace URI " + namespaceURI + " is not supported");
 	}
+
+	private static final Pattern PATTERN_XMLNS = Pattern.compile("http://www\\.dmg\\.org/PMML\\-\\d_\\d");
 }
---
+++ pmml-model/src/test/java/org/dmg/pmml/VersionTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018 Villu Ruusmann
+ */
+package org.dmg.pmml;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class VersionTest {
+
+	@Test
+	public void forNamespaceURI(){
+
+		try {
+			Version.forNamespaceURI("http://www.dmg.org/PMML-2_0");
+
+			fail();
+		} catch(IllegalArgumentException iae){
+			// Ignored;
+		}
+
+		Version.forNamespaceURI("http://www.dmg.org/PMML-3_0");
+		Version.forNamespaceURI("http://www.dmg.org/PMML-3_2");
+		Version.forNamespaceURI("http://www.dmg.org/PMML-4_0");
+		Version.forNamespaceURI("http://www.dmg.org/PMML-4_3");
+
+		try {
+			Version.forNamespaceURI("http://www.dmg.org/PMML-4_4");
+
+			fail();
+		} catch(IllegalArgumentException iae){
+			// Ignored
+		}
+
+		try {
+			Version.forNamespaceURI("http://www.dmg.org/PMML-5_0");
+
+			fail();
+		} catch(IllegalArgumentException iae){
+			// Ignored
+		}
+	}
+}
