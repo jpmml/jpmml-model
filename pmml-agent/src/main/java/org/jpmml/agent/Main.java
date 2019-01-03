ea7bd8d30537ca7120a46dd1df9571b6ebf5c9a0
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-01-03 23:46:11 +0200

    Removed Java agent option 'node'
    See commit 1caa349e12

--- pmml-agent/src/main/java/org/jpmml/agent/Main.java
+++ pmml-agent/src/main/java/org/jpmml/agent/Main.java
@@ -7,10 +7,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.lang.instrument.Instrumentation;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
 import java.util.Properties;
-import java.util.Set;
 
 public class Main {
 
@@ -34,13 +31,6 @@ public class Main {
 		String extensions = properties.getProperty("extensions");
 		if(extensions != null && !("true").equalsIgnoreCase(extensions)){
 			instrumentation.addTransformer(new ExtensionListRemover(), true);
-		}
-
-		String node = properties.getProperty("node");
-		if(node != null){
-			Set<String> commands = new LinkedHashSet<>(Arrays.asList(node.split(",")));
-
-			instrumentation.addTransformer(new NodeTransformer(commands), true);
 		}
 
 		InstrumentationProvider.setInstrumentation(instrumentation);
