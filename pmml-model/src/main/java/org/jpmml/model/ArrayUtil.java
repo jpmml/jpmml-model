0cec954fcdf75bffaec50ca107a3383b32742ee1
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-03-10 01:04:04 +0200

    Refactored ArrayUtil utility class

--- pmml-model/src/main/java/org/jpmml/model/ArrayUtil.java
+++ pmml-model/src/main/java/org/jpmml/model/ArrayUtil.java
@@ -6,16 +6,33 @@ package org.jpmml.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.dmg.pmml.Array;
+import org.dmg.pmml.PrimitiveValueWrapper;
+
 public class ArrayUtil {
 
 	private ArrayUtil(){
 	}
 
 	static
-	public List<String> parse(String string, boolean enableQuotes){
-		List<String> result = new ArrayList<>();
+	public List<String> parse(Array.Type type, String string){
+		List<String> result = new ArrayList<>(Math.max(16, string.length() / 8));
 
 		StringBuilder sb = new StringBuilder();
+
+		boolean enableQuotes;
+
+		switch(type){
+			case INT:
+			case REAL:
+				enableQuotes = false;
+				break;
+			case STRING:
+				enableQuotes = true;
+				break;
+			default:
+				throw new IllegalArgumentException();
+		}
 
 		boolean quoted = false;
 
@@ -39,7 +56,7 @@ public class ArrayUtil {
 					}
 
 					continue tokens;
-				} // End if
+				}
 
 				sb.append(c);
 
@@ -80,6 +97,75 @@ public class ArrayUtil {
 		}
 
 		return result;
+	}
+
+	static
+	public String format(Array.Type type, List<?> objects){
+		StringBuilder sb = new StringBuilder(objects.size() * 16);
+
+		boolean enableQuotes;
+
+		switch(type){
+			case INT:
+			case REAL:
+				enableQuotes = false;
+				break;
+			case STRING:
+				enableQuotes = true;
+				break;
+			default:
+				throw new IllegalArgumentException();
+		}
+
+		for(Object object : objects){
+
+			if(object instanceof PrimitiveValueWrapper){
+				PrimitiveValueWrapper primitiveValueWrapper = (PrimitiveValueWrapper)object;
+
+				object = primitiveValueWrapper.unwrap();
+			}
+
+			String string = object.toString();
+
+			if(sb.length() > 0){
+				sb.append(' ');
+			} // End if
+
+			if(enableQuotes){
+				boolean quoted = ("").equals(string) || (string.indexOf(' ') > -1);
+
+				if(quoted){
+					sb.append('\"');
+				} // End if
+
+				if(string.indexOf('\"') > -1){
+
+					for(int i = 0; i < string.length(); i++){
+						char c = string.charAt(i);
+
+						if(c == '\"'){
+							sb.append('\\');
+						}
+
+						sb.append(c);
+					}
+				} else
+
+				{
+					sb.append(string);
+				} // End if
+
+				if(quoted){
+					sb.append('\"');
+				}
+			} else
+
+			{
+				sb.append(string);
+			}
+		}
+
+		return sb.toString();
 	}
 
 	static
--- pmml-model/src/test/java/org/jpmml/model/ArrayUtilTest.java
+++ pmml-model/src/test/java/org/jpmml/model/ArrayUtilTest.java
@@ -4,8 +4,10 @@
 package org.jpmml.model;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
+import org.dmg.pmml.Array;
+import org.dmg.pmml.PrimitiveValueWrapper;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,36 +16,71 @@ public class ArrayUtilTest {
 
 	@Test
 	public void parseNumberArray(){
-		assertEquals(Arrays.asList("1"), parseNumberArray("1"));
-		assertEquals(Arrays.asList("1.0"), parseNumberArray("1.0"));
+		assertEquals(Arrays.asList("1"), ArrayUtil.parse(Array.Type.INT, "1"));
+		assertEquals(Arrays.asList("1.0"), ArrayUtil.parse(Array.Type.REAL, "1.0"));
 
-		assertEquals(Arrays.asList("1", "2", "3"), parseNumberArray("1 2 3"));
-		assertEquals(Arrays.asList("1.0", "2.0", "3.0"), parseNumberArray("1.0 2.0 3.0"));
+		assertEquals(Arrays.asList("1", "2", "3"), ArrayUtil.parse(Array.Type.INT, "1 2 3"));
+		assertEquals(Arrays.asList("1.0", "2.0", "3.0"), ArrayUtil.parse(Array.Type.REAL, "1.0 2.0 3.0"));
+
+		assertEquals(Arrays.asList("1", "22", "3"), ArrayUtil.parse(Array.Type.INT, "1 22 3"));
+	}
+
+	@Test
+	public void formatNumberArray(){
+		PrimitiveValueWrapper integerTwoWrapper = new PrimitiveValueWrapper(){
+
+			@Override
+			public Object unwrap(){
+				return 2;
+			}
+		};
+
+		assertEquals("1 2 3", ArrayUtil.format(Array.Type.INT, Arrays.asList(1, integerTwoWrapper, 3)));
+		assertEquals("1.0 2.0 3.0", ArrayUtil.format(Array.Type.REAL, Arrays.asList(1d, 2d, 3d)));
+
+		assertEquals("1 22 3", ArrayUtil.format(Array.Type.INT, Arrays.asList("1", "22", "3")));
 	}
 
 	@Test
 	public void parseStringArray(){
-		assertEquals(Arrays.asList("a"), parseStringArray("\"a\""));
+		assertEquals(Collections.emptyList(), ArrayUtil.parse(Array.Type.STRING, ""));
 
-		assertEquals(Arrays.asList("a", "b", "c"), parseStringArray("a b c"));
-		assertEquals(Arrays.asList("a", "b", "c"), parseStringArray("\"a\" \"b\" \"c\""));
+		assertEquals(Arrays.asList(""), ArrayUtil.parse(Array.Type.STRING, "\"\""));
+		assertEquals(Arrays.asList("", " "), ArrayUtil.parse(Array.Type.STRING, "\"\" \" \""));
 
-		assertEquals(Arrays.asList("a b c"), parseStringArray("\"a b c\""));
+		assertEquals(Arrays.asList("a"), ArrayUtil.parse(Array.Type.STRING, "\"a\""));
 
-		assertEquals(Arrays.asList("\"a b c"), parseStringArray("\"a b c"));
-		assertEquals(Arrays.asList("\\a", "\\b\\", "c\\"), parseStringArray("\\a \\b\\ c\\"));
+		assertEquals(Arrays.asList("a", "b", "c"), ArrayUtil.parse(Array.Type.STRING, "a b c"));
+		assertEquals(Arrays.asList("a", "", "b", "c"), ArrayUtil.parse(Array.Type.STRING, "a \"\" b c"));
 
-		assertEquals(Arrays.asList("a \"b\" c"), parseStringArray("\"a \\\"b\\\" c\""));
-		assertEquals(Arrays.asList("\"a b c\""), parseStringArray("\"\\\"a b c\\\"\""));
+		assertEquals(Arrays.asList("a", "b", "c"), ArrayUtil.parse(Array.Type.STRING, "\"a\" \"b\" \"c\""));
+		assertEquals(Arrays.asList("a", "", "b", "c"), ArrayUtil.parse(Array.Type.STRING, "\"a\" \"\" \"b\" \"c\""));
+
+		assertEquals(Arrays.asList("a b c"), ArrayUtil.parse(Array.Type.STRING, "\"a b c\""));
+
+		assertEquals(Arrays.asList("\"a b c"), ArrayUtil.parse(Array.Type.STRING, "\"a b c"));
+		assertEquals(Arrays.asList("\\a", "\\b\\", "c\\"), ArrayUtil.parse(Array.Type.STRING, "\\a \\b\\ c\\"));
+
+		assertEquals(Arrays.asList("a \"b\" c"), ArrayUtil.parse(Array.Type.STRING, "\"a \\\"b\\\" c\""));
+		assertEquals(Arrays.asList("\"a b c\""), ArrayUtil.parse(Array.Type.STRING, "\"\\\"a b c\\\"\""));
+
+		assertEquals(Arrays.asList("ab", "a b", "with \"quotes\" "), ArrayUtil.parse(Array.Type.STRING, "ab  \"a b\"   \"with \\\"quotes\\\" \""));
 	}
 
-	static
-	private List<String> parseNumberArray(String string){
-		return ArrayUtil.parse(string, false);
-	}
+	@Test
+	public void formatStringArray(){
+		assertEquals("", ArrayUtil.format(Array.Type.STRING, Collections.emptyList()));
 
-	static
-	private List<String> parseStringArray(String string){
-		return ArrayUtil.parse(string, true);
+		assertEquals("\"\"", ArrayUtil.format(Array.Type.STRING, Arrays.asList("")));
+		assertEquals("\"\" \" \"", ArrayUtil.format(Array.Type.STRING, Arrays.asList("", " ")));
+
+		assertEquals("a b c", ArrayUtil.format(Array.Type.STRING, Arrays.asList("a", "b", "c")));
+		assertEquals("a \"\" b c", ArrayUtil.format(Array.Type.STRING, Arrays.asList("a", "", "b", "c")));
+
+		assertEquals("\"a b c\"", ArrayUtil.format(Array.Type.STRING, Arrays.asList("a b c")));
+
+		assertEquals("\\a \\b\\ c\\", ArrayUtil.format(Array.Type.STRING, Arrays.asList("\\a", "\\b\\", "c\\")));
+
+		assertEquals("ab \"a b\" \"with \\\"quotes\\\" \"", ArrayUtil.format(Array.Type.STRING, Arrays.asList("ab", "a b", "with \"quotes\" ")));
 	}
 }
