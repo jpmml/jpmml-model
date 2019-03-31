ab8f603bef7806cad80bf83e37b6ba1004345f27
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-03-31 20:52:38 +0300

    Added method HasDataType#getDataType(DataType)

--- pmml-model/src/main/java/org/dmg/pmml/HasDataType.java
+++ pmml-model/src/main/java/org/dmg/pmml/HasDataType.java
@@ -5,6 +5,17 @@ package org.dmg.pmml;
 
 public interface HasDataType<E extends PMMLObject & HasDataType<E>> {
 
+	default
+	DataType getDataType(DataType defaultDataType){
+		DataType dataType = getDataType();
+
+		if(dataType == null){
+			return defaultDataType;
+		}
+
+		return dataType;
+	}
+
 	DataType getDataType();
 
 	E setDataType(DataType dataType);
