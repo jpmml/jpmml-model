88844220e3ab86a598b5bb8a58f17b874f653e30
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-04-01 21:04:18 +0300

    Added method HasOpType#getOpType(OpType)

--- pmml-model/src/main/java/org/dmg/pmml/HasOpType.java
+++ pmml-model/src/main/java/org/dmg/pmml/HasOpType.java
@@ -5,6 +5,17 @@ package org.dmg.pmml;
 
 public interface HasOpType<E extends PMMLObject & HasOpType<E>> {
 
+	default
+	OpType getOpType(OpType defaultOpType){
+		OpType opType = getOpType();
+
+		if(opType == null){
+			return defaultOpType;
+		}
+
+		return opType;
+	}
+
 	OpType getOpType();
 
 	E setOpType(OpType opType);
