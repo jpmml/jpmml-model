bf6a065801bed7a457cafaf99758da21b65f20f3
Villu Ruusmann <villu.ruusmann@gmail.com>
2020-02-18 22:06:43 +0200

    Added Field to HasDisplayValue class hierarchy

--- pmml-model/src/main/java/org/dmg/pmml/Field.java
+++ pmml-model/src/main/java/org/dmg/pmml/Field.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 @XmlTransient
 abstract
-public class Field<E extends Field<E>> extends PMMLObject implements HasName<E>, HasType<E>, Indexable<FieldName> {
+public class Field<E extends Field<E>> extends PMMLObject implements HasName<E>, HasDisplayName<E>, HasType<E>, Indexable<FieldName> {
 
 	@Override
 	public FieldName getKey(){
