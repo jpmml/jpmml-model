860a17a5fd4ebc664d397b6b063d3f501e20f710
Villu Ruusmann <villu.ruusmann@gmail.com>
2020-08-04 13:21:01 +0300

    Added utility method KryoUtil#clone(Kryo, Object)

--- pmml-model-kryo/src/main/java/org/jpmml/model/kryo/KryoUtil.java
+++ pmml-model-kryo/src/main/java/org/jpmml/model/kryo/KryoUtil.java
@@ -3,9 +3,14 @@
  */
 package org.jpmml.model.kryo;
 
+import java.io.Serializable;
+
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import com.esotericsoftware.kryo.serializers.JavaSerializer;
 import org.dmg.pmml.FieldName;
+import org.jpmml.model.DirectByteArrayOutputStream;
 
 public class KryoUtil {
 
@@ -15,5 +20,21 @@ public class KryoUtil {
 	static
 	public void register(Kryo kryo){
 		kryo.register(FieldName.class, new JavaSerializer());
+	}
+
+	@SuppressWarnings (
+		value = {"unchecked"}
+	)
+	static
+	public <S extends Serializable> S clone(Kryo kryo, S object){
+		DirectByteArrayOutputStream buffer = new DirectByteArrayOutputStream(1024 * 1024);
+
+		try(Output output = new Output(buffer)){
+			kryo.writeClassAndObject(output, object);
+		}
+
+		try(Input input = new Input(buffer.getInputStream())){
+			return (S)kryo.readClassAndObject(input);
+		}
 	}
 }
---
+++ pmml-model-kryo/src/test/java/org/jpmml/model/kryo/KryoUtilTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 Villu Ruusmann
+ */
+package org.jpmml.model.kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+import org.dmg.pmml.InlineTableTest;
+import org.dmg.pmml.PMML;
+import org.jpmml.model.MixedContentTest;
+import org.jpmml.model.ResourceUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Element;
+
+public class KryoUtilTest {
+
+	private Kryo kryo = null;
+
+
+	@Before
+	public void setUp(){
+		Kryo kryo = new Kryo();
+		kryo.setReferences(true);
+		kryo.setRegistrationRequired(false);
+		kryo.addDefaultSerializer(Element.class, new JavaSerializer());
+
+		KryoUtil.register(kryo);
+
+		this.kryo = kryo;
+	}
+
+	@Test
+	public void inlineTableTest() throws Exception {
+		PMML pmml = ResourceUtil.unmarshal(InlineTableTest.class);
+
+		KryoUtil.clone(this.kryo, pmml);
+	}
+
+	@Test
+	public void mixedContent() throws Exception {
+		PMML pmml = ResourceUtil.unmarshal(MixedContentTest.class);
+
+		KryoUtil.clone(this.kryo, pmml);
+	}
+}
