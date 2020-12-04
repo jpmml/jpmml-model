15c69cb97803278ed235d52c53ae983b9d2ea74e
Villu Ruusmann <villu.ruusmann@gmail.com>
2020-12-04 21:43:22 +0200

    Added ForwardingModel class

---
+++ pmml-model/src/main/java/org/dmg/pmml/ForwardingModel.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2020 Villu Ruusmann
+ */
+package org.dmg.pmml;
+
+import java.util.Objects;
+
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlTransient
+public class ForwardingModel extends Model {
+
+	private Model model = null;
+
+
+	private ForwardingModel(){
+	}
+
+	public ForwardingModel(Model model){
+		setModel(model);
+	}
+
+	@Override
+	public String getModelName(){
+		return getModel().getModelName();
+	}
+
+	@Override
+	public ForwardingModel setModelName(String modelName){
+		getModel().setModelName(modelName);
+
+		return this;
+	}
+
+	@Override
+	public MiningFunction getMiningFunction(){
+		return getModel().getMiningFunction();
+	}
+
+	@Override
+	public ForwardingModel setMiningFunction(MiningFunction miningFunction){
+		getModel().setMiningFunction(miningFunction);
+
+		return this;
+	}
+
+	@Override
+	public String getAlgorithmName(){
+		return getModel().getAlgorithmName();
+	}
+
+	@Override
+	public ForwardingModel setAlgorithmName(String algorithmName){
+		getModel().setAlgorithmName(algorithmName);
+
+		return this;
+	}
+
+	@Override
+	public boolean isScorable(){
+		return getModel().isScorable();
+	}
+
+	@Override
+	public ForwardingModel setScorable(Boolean scorable){
+		getModel().setScorable(scorable);
+
+		return this;
+	}
+
+	@Override
+	public MathContext getMathContext(){
+		return getModel().getMathContext();
+	}
+
+	@Override
+	public ForwardingModel setMathContext(MathContext mathContext){
+		getModel().setMathContext(mathContext);
+
+		return this;
+	}
+
+	@Override
+	public MiningSchema getMiningSchema(){
+		return getModel().getMiningSchema();
+	}
+
+	@Override
+	public ForwardingModel setMiningSchema(MiningSchema miningSchema){
+		getModel().setMiningSchema(miningSchema);
+
+		return this;
+	}
+
+	@Override
+	public LocalTransformations getLocalTransformations(){
+		return getModel().getLocalTransformations();
+	}
+
+	@Override
+	public ForwardingModel setLocalTransformations(LocalTransformations localTransformations){
+		getModel().setLocalTransformations(localTransformations);
+
+		return this;
+	}
+
+	@Override
+	public Targets getTargets(){
+		return getModel().getTargets();
+	}
+
+	@Override
+	public ForwardingModel setTargets(Targets targets){
+		getModel().setTargets(targets);
+
+		return this;
+	}
+
+	@Override
+	public Output getOutput(){
+		return getModel().getOutput();
+	}
+
+	@Override
+	public ForwardingModel setOutput(Output output){
+		getModel().setOutput(output);
+
+		return this;
+	}
+
+	@Override
+	public ModelStats getModelStats(){
+		return getModel().getModelStats();
+	}
+
+	@Override
+	public ForwardingModel setModelStats(ModelStats modelStats){
+		getModel().setModelStats(modelStats);
+
+		return this;
+	}
+
+	@Override
+	public ModelExplanation getModelExplanation(){
+		return getModel().getModelExplanation();
+	}
+
+	@Override
+	public ForwardingModel setModelExplanation(ModelExplanation modelExplanation){
+		getModel().setModelExplanation(modelExplanation);
+
+		return this;
+	}
+
+	@Override
+	public ModelVerification getModelVerification(){
+		return getModel().getModelVerification();
+	}
+
+	@Override
+	public ForwardingModel setModelVerification(ModelVerification modelVerification){
+		getModel().setModelVerification(modelVerification);
+
+		return this;
+	}
+
+	@Override
+	public VisitorAction accept(Visitor visitor){
+		return getModel().accept(visitor);
+	}
+
+	public Model getModel(){
+		return this.model;
+	}
+
+	private void setModel(Model model){
+		this.model = Objects.requireNonNull(model);
+	}
+}
---
+++ pmml-model/src/test/java/org/dmg/pmml/ForwardingModelTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2020 Villu Ruusmann
+ */
+package org.dmg.pmml;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.dmg.pmml.tree.LeafNode;
+import org.dmg.pmml.tree.Node;
+import org.dmg.pmml.tree.TreeModel;
+import org.jpmml.model.visitors.AbstractVisitor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ForwardingModelTest {
+
+	@Test
+	public void visit(){
+		Node root = new LeafNode(1d, True.INSTANCE);
+
+		TreeModel treeModel = new TreeModel(MiningFunction.REGRESSION, new MiningSchema(), root);
+
+		ForwardingModel forwardingModel = new ForwardingModel(treeModel);
+
+		Set<Model> visitedModels = new HashSet<>();
+		Set<Node> visitedNodes = new HashSet<>();
+
+		Visitor visitor = new AbstractVisitor(){
+
+			@Override
+			public VisitorAction visit(Model model){
+				visitedModels.add(model);
+
+				return super.visit(model);
+			}
+
+			@Override
+			public VisitorAction visit(Node node){
+				visitedNodes.add(node);
+
+				return super.visit(node);
+			}
+		};
+
+		visitor.applyTo(forwardingModel);
+
+		assertEquals(Collections.singleton(treeModel), visitedModels);
+		assertEquals(Collections.singleton(root), visitedNodes);
+
+		forwardingModel = new ForwardingModel(treeModel){
+
+			@Override
+			public VisitorAction accept(Visitor visitor){
+				VisitorAction status = visitor.visit(this);
+
+				return VisitorAction.SKIP;
+			}
+		};
+
+		visitedModels.clear();
+		visitedNodes.clear();
+
+		visitor.applyTo(forwardingModel);
+
+		assertEquals(Collections.singleton(forwardingModel), visitedModels);
+		assertEquals(Collections.emptySet(), visitedNodes);
+	}
+}
