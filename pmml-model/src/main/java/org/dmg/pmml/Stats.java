33282482c1e4e767d72518a1482877bda4eb5a15
Villu Ruusmann <villu.ruusmann@gmail.com>
2021-10-31 10:36:43 +0200

    Added Stats abstract base class

---
+++ pmml-model/src/main/java/org/dmg/pmml/Stats.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 Villu Ruusmann
+ */
+package org.dmg.pmml;
+
+import jakarta.xml.bind.annotation.XmlTransient;
+
+@XmlTransient
+abstract
+public class Stats extends PMMLObject {
+}
