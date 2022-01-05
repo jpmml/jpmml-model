e2af46b93fba3a6bfe57bd04cc682c0e10060c4b
Villu Ruusmann <villu.ruusmann@gmail.com>
2022-01-05 12:20:38 +0200

    Imported Java code from the JPMML-Evaluator project

--- pmml-model/src/main/java/org/jpmml/model/visitors/InvalidMarkupInspector.java
+++ pmml-model/src/main/java/org/jpmml/model/visitors/InvalidMarkupInspector.java
@@ -5,8 +5,20 @@ package org.jpmml.model.visitors;
 
 import java.util.Collection;
 
+import org.dmg.pmml.Apply;
 import org.dmg.pmml.DataDictionary;
+import org.dmg.pmml.Expression;
+import org.dmg.pmml.Interval;
+import org.dmg.pmml.InvalidValueTreatmentMethod;
+import org.dmg.pmml.MiningField;
+import org.dmg.pmml.MissingValueTreatmentMethod;
+import org.dmg.pmml.OutputField;
+import org.dmg.pmml.PMMLAttributes;
 import org.dmg.pmml.PMMLObject;
+import org.dmg.pmml.ResultFeature;
+import org.dmg.pmml.SimplePredicate;
+import org.dmg.pmml.TargetValue;
+import org.dmg.pmml.TextIndexNormalization;
 import org.dmg.pmml.Visitable;
 import org.dmg.pmml.VisitorAction;
 import org.dmg.pmml.association.AssociationModel;
@@ -20,8 +32,11 @@ import org.dmg.pmml.support_vector_machine.Coefficients;
 import org.dmg.pmml.support_vector_machine.SupportVectors;
 import org.dmg.pmml.support_vector_machine.VectorDictionary;
 import org.dmg.pmml.support_vector_machine.VectorFields;
+import org.jpmml.model.InvalidAttributeException;
 import org.jpmml.model.InvalidElementException;
 import org.jpmml.model.InvalidMarkupException;
+import org.jpmml.model.MisplacedAttributeException;
+import org.jpmml.model.MisplacedElementException;
 
 /**
  * <p>
@@ -32,6 +47,21 @@ import org.jpmml.model.InvalidMarkupException;
  * @see InvalidMarkupException
  */
 public class InvalidMarkupInspector extends MarkupInspector<InvalidMarkupException> {
+
+	@Override
+	public VisitorAction visit(Apply apply){
+		InvalidValueTreatmentMethod invalidValueTreatmentMethod = apply.getInvalidValueTreatment();
+
+		switch(invalidValueTreatmentMethod){
+			case AS_VALUE:
+				report(new InvalidAttributeException(apply, invalidValueTreatmentMethod));
+				break;
+			default:
+				break;
+		}
+
+		return super.visit(apply);
+	}
 
 	@Override
 	public VisitorAction visit(AssociationModel associationModel){
@@ -138,6 +168,18 @@ public class InvalidMarkupInspector extends MarkupInspector<InvalidMarkupExcepti
 	}
 
 	@Override
+	public VisitorAction visit(Interval interval){
+		Number leftMargin = interval.getLeftMargin();
+		Number rightMargin = interval.getRightMargin();
+
+		if((leftMargin != null && rightMargin != null) && Double.compare(leftMargin.doubleValue(), rightMargin.doubleValue()) > 0){
+			report(new InvalidElementException(interval));
+		}
+
+		return super.visit(interval);
+	}
+
+	@Override
 	public VisitorAction visit(Itemset itemset){
 		check(new CollectionSize(itemset){
 
@@ -153,6 +195,62 @@ public class InvalidMarkupInspector extends MarkupInspector<InvalidMarkupExcepti
 		});
 
 		return super.visit(itemset);
+	}
+
+	@Override
+	public VisitorAction visit(MiningField miningField){
+		InvalidValueTreatmentMethod invalidValueTreatmentMethod = miningField.getInvalidValueTreatment();
+		Object invalidValueReplacement = miningField.getInvalidValueReplacement();
+
+		switch(invalidValueTreatmentMethod){
+			case AS_IS:
+				// XXX: Non-standard behaviour
+				if(invalidValueReplacement != null){
+					// The JPMML family of libraries introduced invalid value replacement as a vendor extension in PMML schema version 4.3;
+					// the implementation was based on the MiningField@(x-)invalidValueReplacement attribute alone:
+					// <MiningField name="x" invalidValueTreatment="asIs" x-invalidValueReplacement="0"/>
+					// DMG.org introduced invalid value replacement in PMML schema version 4.4;
+					// the implementation uses an invalid value treatment method enum constant "asValue" to signal the availability of the MiningField@invalidValueReplacement attribute:
+					// <MiningField name="x" invalidValueTreatment="asValue" invalidValueReplacement="0"/>
+					// According to the PMML specification, the MiningField@invalidValueReplacement attribute should not be used with the IVTM enum constant "asIs".
+					break;
+				}
+				break;
+			case AS_MISSING:
+			case RETURN_INVALID:
+				if(invalidValueReplacement != null){
+					throw new MisplacedAttributeException(miningField, PMMLAttributes.MININGFIELD_INVALIDVALUEREPLACEMENT, invalidValueReplacement);
+				}
+				break;
+			default:
+				break;
+		}
+
+		MissingValueTreatmentMethod missingValueTreatmentMethod = miningField.getMissingValueTreatment();
+		Object missingValueReplacement = miningField.getMissingValueReplacement();
+
+		if(missingValueTreatmentMethod == null){
+			missingValueTreatmentMethod = MissingValueTreatmentMethod.AS_IS;
+		}
+
+		switch(missingValueTreatmentMethod){
+			case RETURN_INVALID:
+				if(missingValueReplacement != null){
+					report(new MisplacedAttributeException(miningField, PMMLAttributes.MININGFIELD_MISSINGVALUEREPLACEMENT, missingValueReplacement));
+				}
+				break;
+			default:
+				break;
+		}
+
+		Number lowValue = miningField.getLowValue();
+		Number highValue = miningField.getHighValue();
+
+		if((lowValue != null && highValue != null) && Double.compare(lowValue.doubleValue(), highValue.doubleValue()) > 0){
+			throw new InvalidElementException(miningField);
+		}
+
+		return super.visit(miningField);
 	}
 
 	@Override
@@ -228,6 +326,53 @@ public class InvalidMarkupInspector extends MarkupInspector<InvalidMarkupExcepti
 	}
 
 	@Override
+	public VisitorAction visit(OutputField outputField){
+		ResultFeature resultFeature = outputField.getResultFeature();
+		String targetFieldName = outputField.getTargetField();
+
+		if(targetFieldName != null){
+
+			switch(resultFeature){
+				case TRANSFORMED_VALUE:
+				case DECISION:
+					Expression expression = outputField.getExpression();
+					if(expression != null){
+						report(new MisplacedElementException(expression));
+					}
+					// Falls through
+				case WARNING:
+					report(new MisplacedAttributeException(outputField, PMMLAttributes.OUTPUTFIELD_TARGETFIELD, targetFieldName));
+					break;
+				default:
+					break;
+			}
+		}
+
+		return super.visit(outputField);
+	}
+
+	@Override
+	public VisitorAction visit(SimplePredicate simplePredicate){
+		SimplePredicate.Operator operator = simplePredicate.getOperator();
+
+		if(operator != null){
+
+			switch(operator){
+				case IS_MISSING:
+				case IS_NOT_MISSING:
+					if(simplePredicate.hasValue()){
+						report(new MisplacedAttributeException(simplePredicate, PMMLAttributes.SIMPLEPREDICATE_VALUE, simplePredicate.getValue()));
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		return super.visit(simplePredicate);
+	}
+
+	@Override
 	public VisitorAction visit(SupportVectors supportVectors){
 		check(new CollectionSize(supportVectors){
 
@@ -243,6 +388,34 @@ public class InvalidMarkupInspector extends MarkupInspector<InvalidMarkupExcepti
 		});
 
 		return super.visit(supportVectors);
+	}
+
+	@Override
+	public VisitorAction visit(TargetValue targetValue){
+		Number defaultValue = targetValue.getDefaultValue();
+		Object value = targetValue.getValue();
+		Number priorProbability = targetValue.getPriorProbability();
+
+		if(defaultValue != null){
+
+			if(value != null || priorProbability != null){
+				report(new InvalidElementException(targetValue));
+			}
+		}
+
+		return super.visit(targetValue);
+	}
+
+	@Override
+	public VisitorAction visit(TextIndexNormalization textIndexNormalization){
+		String wordRE = textIndexNormalization.getWordRE();
+		String wordSeparatorCharacterRE = textIndexNormalization.getWordSeparatorCharacterRE();
+
+		if(wordRE != null && wordSeparatorCharacterRE != null){
+			report(new InvalidElementException(textIndexNormalization));
+		}
+
+		return super.visit(textIndexNormalization);
 	}
 
 	@Override
--- pmml-model/src/main/java/org/jpmml/model/visitors/MissingMarkupInspector.java
+++ pmml-model/src/main/java/org/jpmml/model/visitors/MissingMarkupInspector.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlElement;
+import org.dmg.pmml.Interval;
+import org.dmg.pmml.InvalidValueTreatmentMethod;
+import org.dmg.pmml.MiningField;
+import org.dmg.pmml.PMMLAttributes;
 import org.dmg.pmml.PMMLObject;
+import org.dmg.pmml.SimplePredicate;
 import org.dmg.pmml.VisitorAction;
 import org.jpmml.model.MissingAttributeException;
 import org.jpmml.model.MissingElementException;
@@ -50,5 +55,61 @@ public class MissingMarkupInspector extends MarkupInspector<MissingMarkupExcepti
 		}
 
 		return super.visit(object);
+	}
+
+	@Override
+	public VisitorAction visit(Interval interval){
+		Number leftMargin = interval.getLeftMargin();
+		Number rightMargin = interval.getRightMargin();
+
+		if(leftMargin == null && rightMargin == null){
+			report(new MissingAttributeException(interval, PMMLAttributes.INTERVAL_LEFTMARGIN));
+			report(new MissingAttributeException(interval, PMMLAttributes.INTERVAL_RIGHTMARGIN));
+		}
+
+		return super.visit(interval);
+	}
+
+	@Override
+	public VisitorAction visit(MiningField miningField){
+		InvalidValueTreatmentMethod invalidValueTreatmentMethod = miningField.getInvalidValueTreatment();
+		Object invalidValueReplacement = miningField.getInvalidValueReplacement();
+
+		switch(invalidValueTreatmentMethod){
+			case AS_VALUE:
+				if(invalidValueReplacement == null){
+					report(new MissingAttributeException(miningField, PMMLAttributes.MININGFIELD_INVALIDVALUEREPLACEMENT));
+				}
+				break;
+			default:
+				break;
+		}
+
+		return super.visit(miningField);
+	}
+
+	@Override
+	public VisitorAction visit(SimplePredicate simplePredicate){
+		SimplePredicate.Operator operator = simplePredicate.getOperator();
+
+		if(operator != null){
+
+			switch(operator){
+				case EQUAL:
+				case NOT_EQUAL:
+				case LESS_THAN:
+				case LESS_OR_EQUAL:
+				case GREATER_OR_EQUAL:
+				case GREATER_THAN:
+					if(!simplePredicate.hasValue()){
+						report(new MissingAttributeException(simplePredicate, PMMLAttributes.SIMPLEPREDICATE_VALUE));
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		return super.visit(simplePredicate);
 	}
 }
