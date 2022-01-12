02bb3a04cb0a36bed6480d346383f30896cd2ed2
Villu Ruusmann <villu.ruusmann@gmail.com>
2022-01-12 11:26:16 +0200

    Added Field to HasRequiredType class hierarchy

--- pmml-model/src/main/java/org/dmg/pmml/Field.java
+++ pmml-model/src/main/java/org/dmg/pmml/Field.java
@@ -7,5 +7,5 @@ import jakarta.xml.bind.annotation.XmlTransient;
 
 @XmlTransient
 abstract
-public class Field<E extends Field<E>> extends PMMLObject implements HasRequiredName<E>, HasDisplayName<E>, HasType<E> {
+public class Field<E extends Field<E>> extends PMMLObject implements HasDisplayName<E>, HasRequiredName<E>, HasRequiredType<E> {
 }
