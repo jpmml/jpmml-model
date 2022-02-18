cacf5167a0b263cf25221b35fa7029d1e3f132ce
Villu Ruusmann <villu.ruusmann@gmail.com>
2022-02-18 13:21:32 +0200

    Added HasModel#requireModel(Class<?>) default method

--- pmml-model/src/main/java/org/dmg/pmml/HasModel.java
+++ pmml-model/src/main/java/org/dmg/pmml/HasModel.java
@@ -3,7 +3,23 @@
  */
 package org.dmg.pmml;
 
+import org.jpmml.model.UnsupportedElementException;
+
 public interface HasModel<E extends PMMLObject & HasModel<E>> {
+
+	/**
+	 * @throws UnsupportedElementException If the {@link Model} child element is not assignment-compatible with the assumed type.
+	 */
+	default
+	<M extends Model> M requireModel(Class<? extends M> clazz){
+		Model model = requireModel();
+
+		if(!clazz.isInstance(model)){
+			throw new UnsupportedElementException(model);
+		}
+
+		return clazz.cast(model);
+	}
 
 	Model requireModel();
 
