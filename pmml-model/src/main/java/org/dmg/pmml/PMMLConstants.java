455aac7ab31d5b9754e44c8e7d5a8728199175e5
Villu Ruusmann <villu.ruusmann@gmail.com>
2022-09-18 15:41:03 +0300

    Added PMMLConstants constant interface

---
+++ pmml-model/src/main/java/org/dmg/pmml/PMMLConstants.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Villu Ruusmann
+ */
+package org.dmg.pmml;
+
+public interface PMMLConstants {
+
+	String NOT_A_NUMBER = "NaN";
+	String POSITIVE_INFINITY = "INF";
+	String NEGATIVE_INFINITY = "-INF";
+}
