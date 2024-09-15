0c9b00669f628be77b90f474f4644e0811bc43a3
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-09-15 11:39:21 +0300

    Extracted VersionUtil utility class

--- pmml-model/src/main/java/org/jpmml/model/visitors/VersionInspector.java
+++ pmml-model/src/main/java/org/jpmml/model/visitors/VersionInspector.java
@@ -6,15 +6,13 @@ package org.jpmml.model.visitors;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.dmg.pmml.Apply;
 import org.dmg.pmml.PMMLAttributes;
-import org.dmg.pmml.PMMLFunctions;
 import org.dmg.pmml.PMMLObject;
 import org.dmg.pmml.Version;
+import org.dmg.pmml.VersionUtil;
 import org.dmg.pmml.VisitorAction;
 import org.jpmml.model.ReflectionUtil;
 import org.jpmml.model.annotations.Added;
@@ -75,7 +73,7 @@ public class VersionInspector extends AbstractVisitor {
 	public VisitorAction visit(Apply apply){
 		String function = apply.requireFunction();
 
-		Version version = VersionInspector.functionVersions.get(function);
+		Version version = VersionUtil.getVersion(function);
 		if(version != null){
 			updateMinimum(apply, PMMLAttributes.APPLY_FUNCTION, version);
 		}
@@ -141,49 +139,5 @@ public class VersionInspector extends AbstractVisitor {
 		Version[] values = Version.values();
 
 		return values[version.ordinal() - 1];
-	}
-
-	private static Map<String, Version> functionVersions = new LinkedHashMap<>();
-
-	static {
-		declareFunctions(Version.PMML_3_0,
-			PMMLFunctions.ADD, PMMLFunctions.SUBTRACT, PMMLFunctions.MULTIPLY, PMMLFunctions.DIVIDE,
-			PMMLFunctions.MIN, PMMLFunctions.MAX, PMMLFunctions.SUM, PMMLFunctions.AVG,
-			PMMLFunctions.LOG10, PMMLFunctions.LN, PMMLFunctions.SQRT, PMMLFunctions.ABS,
-			PMMLFunctions.UPPERCASE, PMMLFunctions.SUBSTRING, PMMLFunctions.TRIMBLANKS,
-			PMMLFunctions.FORMATNUMBER, PMMLFunctions.FORMATDATETIME,
-			PMMLFunctions.DATEDAYSSINCEYEAR, PMMLFunctions.DATESECONDSSINCEYEAR, PMMLFunctions.DATESECONDSSINCEMIDNIGHT);
-		declareFunctions(Version.PMML_3_1,
-			PMMLFunctions.EXP, PMMLFunctions.POW, PMMLFunctions.THRESHOLD, PMMLFunctions.FLOOR, PMMLFunctions.CEIL, PMMLFunctions.ROUND);
-		declareFunctions(Version.PMML_4_0,
-			PMMLFunctions.ISMISSING, PMMLFunctions.ISNOTMISSING,
-			PMMLFunctions.EQUAL, PMMLFunctions.NOTEQUAL, PMMLFunctions.LESSTHAN, PMMLFunctions.LESSOREQUAL, PMMLFunctions.GREATERTHAN, PMMLFunctions.GREATEROREQUAL,
-			PMMLFunctions.AND, PMMLFunctions.OR,
-			PMMLFunctions.NOT,
-			PMMLFunctions.ISIN, PMMLFunctions.ISNOTIN,
-			PMMLFunctions.IF);
-		declareFunctions(Version.PMML_4_1,
-			PMMLFunctions.MEDIAN, PMMLFunctions.PRODUCT,
-			PMMLFunctions.LOWERCASE);
-		declareFunctions(Version.PMML_4_2,
-			PMMLFunctions.CONCAT, PMMLFunctions.REPLACE, PMMLFunctions.MATCHES);
-		declareFunctions(Version.PMML_4_3,
-			PMMLFunctions.NORMALCDF, PMMLFunctions.NORMALPDF, PMMLFunctions.STDNORMALCDF, PMMLFunctions.STDNORMALPDF, PMMLFunctions.ERF, PMMLFunctions.NORMALIDF, PMMLFunctions.STDNORMALIDF);
-		declareFunctions(Version.PMML_4_4,
-			PMMLFunctions.MODULO,
-			PMMLFunctions.ISVALID, PMMLFunctions.ISNOTVALID,
-			PMMLFunctions.EXPM1, PMMLFunctions.HYPOT, PMMLFunctions.LN1P, PMMLFunctions.RINT,
-			PMMLFunctions.STRINGLENGTH,
-			PMMLFunctions.SIN, PMMLFunctions.ASIN, PMMLFunctions.SINH, PMMLFunctions.COS, PMMLFunctions.ACOS, PMMLFunctions.COSH, PMMLFunctions.TAN, PMMLFunctions.ATAN, PMMLFunctions.TANH);
-		declareFunctions(Version.XPMML,
-			PMMLFunctions.ATAN2);
-	}
-
-	static
-	private void declareFunctions(Version version, String... functions){
-
-		for(String function : functions){
-			VersionInspector.functionVersions.put(function, version);
-		}
 	}
 }
