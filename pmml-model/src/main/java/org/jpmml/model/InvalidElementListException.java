ad20c9ee34c138edc464f2f07cee6aa2d26b88b8
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-11-11 22:11:13 +0200

    Added MisplacedElementListException exception class

--- pmml-model/src/main/java/org/jpmml/model/InvalidElementListException.java
+++ pmml-model/src/main/java/org/jpmml/model/InvalidElementListException.java
@@ -9,6 +9,14 @@ import org.dmg.pmml.PMMLObject;
 
 public class InvalidElementListException extends InvalidMarkupException {
 
+	public InvalidElementListException(String message){
+		super(message);
+	}
+
+	public InvalidElementListException(String message, PMMLObject object){
+		super(message, object);
+	}
+
 	public InvalidElementListException(List<? extends PMMLObject> objects){
 		super("List of elements " + XPathUtil.formatElement((objects.get(0)).getClass()) + " is not valid", objects.get(0));
 	}
